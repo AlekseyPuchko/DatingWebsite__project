a2d8b9e036d1fd0411ff7ae1d1aa3f31449d7283
Aleksey Puchko <aleshalinguini@gmail.com>
2021-04-26 00:34:29 +0300

    add news.html, places.html
    add news.html, places.html

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 $(document).ready(function () {
     $(".slider").slick();
 });
+
+$(document).ready(function () {
+    $(".cards").slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        variableWidth: true,
+        swipe: true,
+        dots: true,
+    });
+});
