f5d41dd13a9e9171dc883109d82daaa4b8b39473
Aleksey Puchko <aleshalinguini@gmail.com>
2021-04-26 00:36:32 +0300

    Revert "add news.html, places.html"
    This reverts commit a2d8b9e036d1fd0411ff7ae1d1aa3f31449d7283.

--- js/script.js
+++ js/script.js
@@ -1,15 +1,3 @@
 $(document).ready(function () {
     $(".slider").slick();
 });
-
-$(document).ready(function () {
-    $(".cards").slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        variableWidth: true,
-        swipe: true,
-        dots: true,
-    });
-});
